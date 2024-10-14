050a51443319983a8656325d086579624920bddb
Nate <naref@stepnetwork.dk>
2024-10-14 12:34:22 +0200

    Testing basic Hello world

--- server.js
+++ server.js
@@ -1,28 +1,10 @@
 const express = require('express');
-const cors = require('cors');
-const path = require('path');
 const app = express();
-
-app.use(cors({
-    origin: '*'
-}));
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    // res.send("Hello, World");
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-app.post('/receive-nexta-content', (req, res) => {
-    console.log('Received nextaContent:', JSON.stringify(req.body));
-    const { nextaContent } = req.body;
-    if (nextaContent) {
-        console.log('Received nextaContent:', nextaContent);
-        // Further processing...
-        res.status(200).send('nextaContent received successfully');
-    } else {
-        res.status(400).send('nextaContent is missing');
-    }
+    res.send("Hello, World");
 });
 
 const PORT = process.env.PORT || 3000;
