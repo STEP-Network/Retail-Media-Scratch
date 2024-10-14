39b672c4cb9f105f36e20faf10ed05f25abc021c
Nate <naref@stepnetwork.dk>
2024-10-14 10:31:16 +0200

    Fixed issues - now ad is loading

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    // res.send("Hello, World");
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.post('/receive-nexta-content', (req, res) => {
     console.log('Received nextaContent:', JSON.stringify(req.body));
